5d0f0a2f82cc9cb1220cf3b6c2bae8bba6950185
Kayke Barbosa <kaykeloiola@gmail.com>
2023-05-05 07:41:41 -0300

    App

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,22 @@
+import { HeaderGreen, HeaderYellow } from "./styled"
+const Home = () => {
+  return (
+    <>
+    <HeaderGreen>
+      <h1>⠀</h1>
+    </HeaderGreen>
+
+    <HeaderYellow>
+      <h1>⠀</h1>
+    </HeaderYellow>
+
+    <div>
+      
+    </div>
+    </>
+  
+
+  )
+} 
+
+export default Home
---
+++ src/components/Home/styled.jsx
@@ -0,0 +1,11 @@
+import  styled from 'styled-components'
+
+export const HeaderGreen = styled.header`
+  background-color: #2AB572;
+  width: 100%;
+`;
+
+export const HeaderYellow = styled.header`
+  background-color: #FFBA00;
+  width: 100%;
+`;
